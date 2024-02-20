fc18a7887c7784fbd946e064b91cf23107412717
nlok5923 <nlokhande5923@gmail.com>
2024-02-20 13:11:21 +0530

    proofs verifier

--- index.js
+++ index.js
@@ -3,6 +3,34 @@ const express = require('express');
 const fs = require('fs');
 const cors = require('cors');
 const path = require('path');
+const { ethers } = require('ethers');
+require('dotenv').config();
+
+const provider = new ethers.providers.JsonRpcProvider('https://stylus-testnet.arbitrum.io/rpc');
+const contractAddress = '0xc574c3ca325A949BD3cf2c11BA5Bdd8fCCcb3Ad4';
+const contractABI = [
+  {
+      "inputs": [
+          {
+              "internalType": "uint256[9]",
+              "name": "words",
+              "type": "uint256[9]"
+          }
+      ],
+      "name": "verifyProof",
+      "outputs": [
+          {
+              "internalType": "bool",
+              "name": "",
+              "type": "bool"
+          }
+      ],
+      "stateMutability": "pure",
+      "type": "function"
+  }
+];
+
+const contract = new ethers.Contract(contractAddress, contractABI, provider);
 
 const corsOptions = {
     origin:
@@ -15,12 +43,9 @@ const app = express();
 app.use(express.json());
 app.use(cors(corsOptions));
 
-const generateProof = async (input_img) => {
-    // console.log('This is context ', ctx);
+const generateProof = async (input_img, operation) => {
     const img_data = JSON.parse(input_img);
-    // let input;
 
-    console.log('This is context ', img_data);
     const image_array = img_data.input;
 
     const rows = 128;
@@ -39,25 +64,40 @@ const generateProof = async (input_img) => {
         }
     }
 
-    console.log(image);
+    // console.log(image);
 
     let input = {
-        "option": "1",
+        "option": operation,
         "orig": image,
     }
 
-    console.log(input);
+    // console.log(input);
     const { proof, publicSignals } = await snarkjs.groth16.fullProve(input, 'circuit.wasm', 'circuit_final.zkey');
-
-    console.log('proof', proof);
-    console.log("signals", publicSignals);
 
     return proof;
 }
 
 app.get('/generate-proof', async (req, res) => {
     const body = req.body;
-    const response = await generateProof(body.input);
+    const operation = JSON.parse(req.body.input).operation;
+    const response = await generateProof(body.input, operation);
+
+    // disabled proof verification now
+    
+    // const proofCalldata = await snarkjs.groth16.exportSolidityCallData(response, [operation]);
+    // console.log(proofCalldata);
+
+
+    // const regex = /0x[\da-fA-F]+/g;
+    
+    // const parsedProof = proofCalldata.match(regex);
+    
+    // const parsedProof = JSON.parse(JSON.stringify(sample));
+    // console.log("proof", parsedProof);
+
+    // const isVerified = await contract.verifyProof(parsedProof);
+    // console.log(isVerified)
+
     res.send(JSON.stringify({ message: response }));
 })
 
